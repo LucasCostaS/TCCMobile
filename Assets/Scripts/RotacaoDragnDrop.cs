4ca4cee307eaea11ce6952dbb15c1a2f4fff9bf4
LucasCostaS <50673347+LucasCostaS@users.noreply.github.com>
2022-11-08 22:01:07 -0300

    Rotacao do drag n drop Fase2 (Arrumado)

--- Assets/Scripts/RotacaoDragnDrop.cs
+++ Assets/Scripts/RotacaoDragnDrop.cs
@@ -1,53 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RotacaoDragnDrop : MonoBehaviour
 {
+    EventSystem m_EventSystem;
 
+    void OnEnable()
+    {
+        //Fetch the current EventSystem. Make sure your Scene has one.
+        m_EventSystem = EventSystem.current;
+    }
 
+    private Touch touch;
     Vector3 touchPosWorld;
-    Vector2 iniTouchPosWorld2D, endTouchPosWorld2D;
-
-    //Change me to change the touch phase used.
-    TouchPhase touchPhase = TouchPhase.Ended;
+    private float touchDuration;
 
     void Update()
     {
 
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0 )
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            touchDuration += Time.deltaTime;
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended && touchDuration < 0.2f) 
             {
                 touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
-                Vector2 iniTouchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
-            }
+                Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
 
-            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
 
-                Vector2 endTouchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
-            }
-            else
-            {
-                endTouchPosWorld2D = new Vector2(0f, 0f);
-            }
-            if (iniTouchPosWorld2D == endTouchPosWorld2D)
-            {
-                //We should have hit something with a 2D Physics collider!
-                //GameObject touchedObject = hitInformation.transform.gameObject;
-                //touchedObject should be the object someone touched.
-                gameObject.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
-                //Debug.Log(touchedObject.transform.eulerAngles.z);
-            }
+                if (hitInformation.collider != null)
+                {
+
+                    GameObject touchedObject = hitInformation.transform.gameObject;
+
+                    // possibilidade de ter que usar try-catch
+                    if(touchedObject.transform.parent.transform.parent.name == "pecas")
+                        touchedObject.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
+
+                }
+            }  
         }
-            //We now raycast with this information. If we have hit something we can process it.
-            //RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
-
-            //if (hitInformation.collider != null)
-            
-         
+        else
+            touchDuration = 0.0f;
     }
 }
